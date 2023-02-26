c8c521b4c4efbddcd06bbd5b20a78d9aba08b798
Shobhandeb <devshobhan4@gmail.com>
2023-02-27 01:38:29 +0530

    SMA

--- Stocks-temp-dump/Stock_Predictor_WebApp/app.py
+++ Stocks-temp-dump/Stock_Predictor_WebApp/app.py
@@ -17,17 +17,19 @@ import technical_analysis as t
 import matplotlib.pyplot as plt
 import mpld3
 import streamlit.components.v1 as components
-
+from ta.trend import MACD
+from ta.momentum import RSIIndicator
+from ta.volatility import BollingerBands
 
 
 with st.sidebar:
+
     st.markdown("# Stock Analysis & Forecasting")
     user_input = st.text_input('Enter Stock Name', "ADANIENT.NS")
     st.markdown("### Choose Date for your anaylsis")
     date_from = st.date_input("From",datetime.date(2020, 1, 1))
     date_to = st.date_input("To",datetime.date(2023, 2, 25))
-    btn = st.button('Submit')
-
+    btn = st.button('Submit') 
 
 #adding a button
 if btn:
@@ -73,26 +75,81 @@ if btn:
     t.volume_plot(df)
     st.pyplot(fig)
 
-    # st.markdown("### Correlation Plot")
-    # t.correlation_plot(df)
-    # fig, ax = plt.subplots(figsize=(20,10))
-    # ax.set_title('Correlation Plot')
-    # ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
-    # ax.set_yticklabels(ax.get_yticklabels(), rotation=0, ha='right')
-    # plt.tight_layout()
-    # st.pyplot(fig)
+    st.markdown("### Volume Plot")
+    fig= plt.figure(figsize=(20,10))
+    t.correlation_plot(df)
+    st.pyplot(fig)
 
-    
     st.markdown("### Volatility Plot")
     fig= plt.figure(figsize=(20,10))
-    t.volatility(df)
+    t.volatility_plot(df)
     st.pyplot(fig)
 
 
+    st.markdown("# Technical Analysis")
 
+    st.markdown("## MACD Indicator")
+    
+    fig= plt.figure(figsize=(20,10))
+    t.plot_price_and_signals(t.get_macd(df),'MACD')
+    st.pyplot(fig)
+
+    fig= plt.figure(figsize=(20,10))
+    t.plot_macd(df)
+    st.pyplot(fig)
+
+    st.write(" ***:blue[Strategy:]:***")
+    st.write(":red[Sell  Signal:] The cross over: When the MACD line is below the signal line.")
+    st.write(":green[Buy Signal:] The cross over: When the MACD line is above the signal line.")
+
+    st.markdown("## RSI Indicator")
+
+    fig= plt.figure(figsize=(20,10))
+    t.plot_price_and_signals(t.get_rsi(df),'RSI')
+    st.pyplot(fig)
+
+    fig= plt.figure(figsize=(20,10))
+    t.plot_rsi(df)
+    st.pyplot(fig)
+
+    st.write(" ***:blue[Strategy:]:***")
+    st.write(":red[Sell  Signal:] When RSI increases above 70%")
+    st.write(":green[Buy Signal:] When RSI decreases below 30%.")
+
+
+    st.markdown("## Bollinger Indicator")
+
+    fig= plt.figure(figsize=(20,10))
+    t.plot_price_and_signals(t.get_bollinger_bands(df),'Bollinger_Bands')
+    st.pyplot(fig)
+
+    fig= plt.figure(figsize=(20,10))
+    t.plot_bollinger_bands(df)
+    st.pyplot(fig)
+
+    st.write(" ***:blue[Strategy:]:***")
+    st.write(":red[Sell  Signal:] As soon as the market price touches the upper Bollinger band")
+    st.write(":green[Buy Signal:] As soon as the market price touches the lower Bollinger band")
 
 else:
     st.write('Please click on the submit to get the analysis') #displayed when the button is unclicked
 
     
-        
+
+    st.markdown("## SMA Indicator")
+    # Get the data and signals for the selected stock
+    data = t.get_signals(user_input)
+    df, sma, buy_sell_df = t.get_signals(data)
+
+    # Create a plot of the stock price with sma and buy and sell signals
+    fig, ax = plt.subplots(figsize=(20, 9))
+    ax.plot(df.index, df['Close'], label='Close')
+    ax.plot(sma.index, sma, label='SMA')
+    ax.scatter(buy_sell_df.index, buy_sell_df['Buy'], marker='^', color='green', label='Buy')
+    ax.scatter(buy_sell_df.index, buy_sell_df['Sell'], marker='v', color='red', label='Sell')
+    ax.legend()
+    st.pyplot(fig)
+
+
+
+
--- Stocks-temp-dump/Stock_Predictor_WebApp/technical_analysis.py
+++ Stocks-temp-dump/Stock_Predictor_WebApp/technical_analysis.py
@@ -2,21 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as st
 import yfinance as yf
 import datetime
 import warnings
+from ta.trend import MACD
+from ta.momentum import RSIIndicator
+from ta.volatility import BollingerBands
 warnings.filterwarnings('ignore')
 
-
-# @st.cache(suppress_st_warning=True)
 def calculated_df(df):
     df['Date'] = df.index
     df['Day_Perc_Change'] = df['Adj Close'].pct_change()*100
     df.dropna(inplace= True)
     df['Trend']= np.zeros(df['Day_Perc_Change'].count())
     df['Trend']= df['Day_Perc_Change'].apply(lambda x:trend(x))
-    return df 
+    return df
 
 def last_2_years_price_plot(df):
     df['Adj Close'].plot()
@@ -61,31 +61,176 @@ def trend_pie_chart(df):
     plt.pie(calculated_df(df)['Trend'].value_counts(), labels = pie_label, autopct = '%1.1f%%', radius = 2)
     plt.show()
 
-# def volume_plot(df):
-#     plt.stem(calculated_df(df)['Date'], calculated_df(df)['Day_Perc_Change'])
-#     (calculated_df(df)/1000000).plot(figsize = (15, 7.5), color = 'green', alpha = 0.5)
-# Daily volume of trade has been reduced in scale to match with the daily return scale
-
 def volume_plot(df):
-    calculated = calculated_df(df)
-    plt.stem(calculated['Date'], calculated['Day_Perc_Change'])
-    (df['Volume']/1000000).plot(figsize=(15, 7.5), color='green', alpha=0.5)
-    # Daily volume of trade has been reduced in scale to match with the daily return 
-    
-# def correlation_plot(df):
-#     # calculated = calculated_df(df)
-#     selected_stocks = st.multiselect('Select stocks', ['AAPL', 'MSFT', 'GOOG', 'AMZN'])
-#     df = yf.download(selected_stocks, start='2010-01-01', end='2022-02-25')
-#     corr_matrix = df.corr()
-#     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1, center=0)
-    
- 
-    
-def volatility(df):
-    calculated = calculated_df(df)
+    # Daily volume of trade has been reduced in scale to match with the daily return scale
+    calculated_data = calculated_df(df)
+    plt.stem(calculated_data['Date'], calculated_data['Day_Perc_Change'])
+    (calculated_data['Volume']/1000000).plot(figsize = (15, 7.5), color = 'green', alpha = 0.5)
+    plt.show()
 
-    calculated['Daily_Return'] = calculated['Adj Close'].pct_change()
 
-    # Calculate the rolling standard deviation (volatility) over 30 days
-    calculated['Volatility'] = calculated['Daily_Return'].rolling(window=30).std() * (252**0.5)
+def correlation_plot(df):
+    # Adj close price of all the stocks
+    combined_df = yf.download(["ADANIENT.NS","TATASTEEL.NS","PAGEIND.NS","EICHERMOT.NS","INFY.NS"], start="2010-01-01", end="2023-02-12")['Adj Close']
+    combined_df = combined_df.round(2)
+    combined_df.head()
+    
+def volatility_plot(df):
+    ADANI_vol = calculated_df(df)['Day_Perc_Change'].rolling(7).std()*np.sqrt(7)
+    ADANI_vol.plot()
 
+
+def generate_buy_sell_signals(condition_buy, condition_sell, dataframe, strategy):
+    last_signal = None
+    indicators = []
+    buy = []
+    sell = []
+    for i in range(0, len(dataframe)):
+        # if buy condition is true and last signal was not Buy
+        if condition_buy(i, dataframe) and last_signal != 'Buy':
+            last_signal = 'Buy'
+            indicators.append(last_signal)
+            buy.append(dataframe['Close'].iloc[i])
+            sell.append(np.nan)
+        # if sell condition is true and last signal was Buy
+        elif condition_sell(i, dataframe)  and last_signal == 'Buy':
+            last_signal = 'Sell'
+            indicators.append(last_signal)
+            buy.append(np.nan)
+            sell.append(dataframe['Close'].iloc[i])
+        else:
+            indicators.append(last_signal)
+            buy.append(np.nan)
+            sell.append(np.nan)
+
+    dataframe[f"{strategy}_Last_Signal"] = np.array(last_signal)
+    dataframe[f"{strategy}_Indicator"] = np.array(indicators)
+    dataframe[f"{strategy}_Buy"] = np.array(buy)
+    dataframe[f"{strategy}_Sell"] = np.array(sell)
+
+
+def get_macd(company):
+    close_prices = company['Close']
+    window_slow = 26
+    signal = 9
+    window_fast = 12
+    macd = MACD(close_prices, window_slow, window_fast, signal)
+    company['MACD'] = macd.macd()
+    company['MACD_Histogram'] = macd.macd_diff()
+    company['MACD_Signal'] = macd.macd_signal()
+
+    generate_buy_sell_signals(
+    lambda x, company: company['MACD'].values[x] < company['MACD_Signal'].iloc[x],
+    lambda x, company: company['MACD'].values[x] > company['MACD_Signal'].iloc[x],
+    company,
+    'MACD')
+    return company
+
+def plot_price_and_signals(company, strategy):
+    # data = get_macd(company)
+    data = company
+    last_signal_val = data[f"{strategy}_Last_Signal"].values[-1]
+    last_signal = 'Unknown' if not last_signal_val else last_signal_val
+    title = f'Close Price Buy/Sell Signals using {strategy}.  Last Signal: {last_signal}'
+    # fig.suptitle(f'Top: ADANI Stock Price. Bottom: {strategy}')
+    if not data[f'{strategy}_Buy'].isnull().all():
+        plt.scatter(data.index, data[f'{strategy}_Buy'], color='green', label='Buy Signal', marker='^', alpha=1)
+    if not data[f'{strategy}_Sell'].isnull().all():
+        plt.scatter(data.index, data[f'{strategy}_Sell'], color='red', label='Sell Signal', marker='v', alpha=1)
+    plt.plot(company['Close'], label='Close Price', color='blue', alpha=0.35)
+    plt.xticks(rotation=45)
+    plt.title(title)
+    plt.xlabel('Date', fontsize=20)
+    plt.ylabel('Close Price', fontsize=18)
+    plt.legend(loc='upper left')
+
+
+def plot_macd(company):
+    macd = get_macd(company)
+    plt.plot(macd['MACD'], label=' MACD', color = 'green')
+    plt.plot(macd['MACD_Signal'], label='Signal Line', color='orange')
+    positive = macd['MACD_Histogram'][(macd['MACD_Histogram'] >= 0)]
+    negative = macd['MACD_Histogram'][(macd['MACD_Histogram'] < 0)]
+    plt.bar(positive.index, positive, color='green')
+    plt.bar(negative.index, negative, color='red')    
+    plt.legend(loc='upper left')
+    plt.show()
+
+def get_rsi(company):
+    close_prices = company['Close']
+    # dataframe = company.technical_indicators
+    rsi_time_period = 20
+
+    rsi_indicator = RSIIndicator(close_prices, rsi_time_period)
+    company['RSI'] = rsi_indicator.rsi()
+
+    low_rsi = 40
+    high_rsi = 70
+
+    generate_buy_sell_signals(
+        lambda x, company: company['RSI'].values[x] < low_rsi,
+        lambda x, company: company['RSI'].values[x] > high_rsi,
+    company, 'RSI')
+
+    return company
+
+def plot_rsi(company):
+    rsi = get_rsi(company)
+    low_rsi = 40
+    high_rsi = 70
+    plt.fill_between(rsi.index, y1=low_rsi, y2=high_rsi, color='#adccff', alpha=0.3)
+    plt.plot(rsi['RSI'], label='RSI', color='blue', alpha=0.35)
+    plt.legend(loc='upper left')
+    plt.show()
+
+def get_bollinger_bands(company):
+    close_prices = company['Close']
+    window = 20
+    indicator_bb = BollingerBands(close=close_prices, window=window, window_dev=2)
+
+    # Add Bollinger Bands features
+    company['Bollinger_Bands_Middle'] = indicator_bb.bollinger_mavg()
+    company['Bollinger_Bands_Upper'] = indicator_bb.bollinger_hband()
+    company['Bollinger_Bands_Lower'] = indicator_bb.bollinger_lband()
+
+    generate_buy_sell_signals(
+        lambda x, company: company['Close'].values[x] < company['Bollinger_Bands_Lower'].values[x],
+        lambda x, company: company['Close'].values[x] > company['Bollinger_Bands_Upper'].values[x],
+        company, 'Bollinger_Bands')
+
+    return company
+
+def plot_bollinger_bands(company):
+        bollinger_bands = get_bollinger_bands(company)
+
+        # fig, axs = plt.subplots(2, sharex=True, figsize=(20, 8))
+        # plot_price_and_signals(fig, company, bollinger_bands, 'Bollinger_Bands', axs)
+        plt.plot(bollinger_bands['Bollinger_Bands_Middle'], label='Middle', color='blue', alpha=0.35)
+        plt.plot(bollinger_bands['Bollinger_Bands_Upper'], label='Upper', color='green', alpha=0.35)
+        plt.plot(bollinger_bands['Bollinger_Bands_Lower'], label='Lower', color='red', alpha=0.35)
+        plt.fill_between(bollinger_bands.index, bollinger_bands['Bollinger_Bands_Lower'], bollinger_bands['Bollinger_Bands_Upper'], alpha=0.1)
+        plt.legend(loc='upper left')
+        plt.show()
+
+
+    
+def get_signals(data):
+    df = data.iloc[-252:]
+    sma = df['Close'].rolling(window=50).mean()
+
+    buy_sell_df = pd.DataFrame(index=df.index)
+    buy_sell_df['SMA'] = sma
+    buy_sell_df['Close'] = df['Close']
+    buy_sell_df['Signal'] = 0.0
+    buy_sell_df['Buy'] = np.zeros(len(df))
+    buy_sell_df['Sell'] = np.zeros(len(df))
+
+    for i in range(1, len(df)):
+        if sma[i] > df['Close'][i] and sma[i-1] <= df['Close'][i-1]:
+            buy_sell_df['Signal'][i] = 1.0
+            buy_sell_df['Buy'][i] = df['Close'][i]
+        elif sma[i] < df['Close'][i] and sma[i-1] >= df['Close'][i-1]:
+            buy_sell_df['Signal'][i] = -1.0
+            buy_sell_df['Sell'][i] = df['Close'][i]
+
+    return df, sma, buy_sell_df
